700032876c4b1bd78e50a3de01230eaf5e38f5fb
Elbert (Sangyoup) Bae <elbert.bae@gmail.com>
2022-03-14 11:18:34 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,59 +1,12 @@
-/* eslint-disable */
-import { useState } from "react";
-import HistoryList from "./components/history-list/history-list";
-import debounce from "lodash.debounce";
-import "./styles.css";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
-const DEBOUNCE_DELAY = 500;
+import App from "./App";
 
-export default function App() {
-  const [str, setStr] = useState(null);
-  const [searchHistory, setSearchHistory] = useState([]);
-
-  return (
-    <div className="App">
-      <h1>Search Something</h1>
-      <input
-        type="text"
-        value={str}
-        onChange={(e) => {
-          setStr(e.target.value);
-        }}
-        placeholder={"Type something..."}
-      />
-
-      <button
-        type="button"
-        onClick={() => setSearchHistory((i) => [...i, str])}
-      >
-        Search
-      </button>
-
-      <button
-        type="button"
-        onClick={() => {
-          const newHistory = searchHistory;
-          newHistory.pop();
-          setSearchHistory(newHistory);
-
-          const result = newHistory.length;
-          if (result == 0) {
-            setStr(null);
-          } else {
-            setStr(newHistory[newHistory.length - 1]);
-          }
-        }}
-      >
-        Undo
-      </button>
-
-      <h3>Search History</h3>
-      <HistoryList
-        history={searchHistory}
-        onItemClick={(search) => {
-          setStr(search);
-        }}
-      />
-    </div>
-  );
-}
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
